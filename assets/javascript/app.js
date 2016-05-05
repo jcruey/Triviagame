e4d810236afb2634e28a676dfa306b9fd360857c
Ksenia Cruey <Joshua@crueyphotography.com>
2016-05-04 22:01:49 -0400

    organized game JS into object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,132 +4,162 @@
 
     $(document).ready(function() {
 
-    var correct = 0;
-    var incorrect = 0;
-    var askedQuestions = 0;
-    var number = 30;
-
-
     //question and array
-    var questions = [
-    {
-        question: "Pteronophobia is the fear of what?",
-        correctAnswer: "Being tickled by feathers",
-        answer1: "Being tickled by fingers",
-        answer2: "Being tickled by Hair",
-    },{
-        question: "Saliva has a boiling point ___ times higher than water?",
-        answer1: "Two times",
-        answer2: "Five times",
-        correctAnswer: "Three times",
-    },{
-        question: "Heart Attacks are most likely to occur on this day of the week?",
-        answer1: "Tuesday",
-        answer2Answer: "Monday",
-        choice2: "Friday",
-    },{
-        question: "How many Americans are injured by Musical Instruments each year?",
-        answer1: "12000",
-        answer2:"6000",
-        correctAnswer: "8000",
-    },{
-        question : "How many steps does it take to reach the top of the Eiffel Tower?",
-        correctAnswer : "1665",
-        answer1 : "1710",
-        answer2 : "1980",
-    },{
-        question : "What is the national animal of Scotland",
-        answer1 : "Gyrfalcon",
-        answer2Answer : "Unicorn",
-        choice2 : "Chollima",
-    },{
-        question : "How many beaches does Australia have?",
-        answer1 : "12,400",
-        answer2Answer : "10,685",
-        choice2 : "6,942",
-    },{
-        question : "A grasshopper can leap ____ times the length of it's body?",
-        answer1 : "10",
-        answer2Answer : "20",
-        choice2 : "36",
-    },{
-        question : "The average iPhone user unlocks their device ___ times per day?",
-        correctAnswer : "80",
-        answer1 : "30",
-        answer2 : "140",
-    }];
+    var game = {
+        correct: 0,
+        incorrect: 0,
+        askedQuestions: 0,
+        number: 30,
+        answerTime: 15,
+    
+        questions:[{
+                    question: "Pteronophobia is the fear of what?",
+                    correctAnswer: "Being tickled by feathers",
+                    answer1: "Being tickled by fingers",
+                    answer2: "Being tickled by Hair",
+                    },{
+                    question: "Saliva has a boiling point ___ times higher than water?",
+                    answer1: "Two times",
+                    answer2: "Five times",
+                    correctAnswer: "Three times",
+                    },{
+                    question: "Heart Attacks are most likely to occur on this day of the week?",
+                    answer1: "Tuesday",
+                    correctAnswer: "Monday",
+                    answer2: "Friday",
+                    },{
+                    question: "How many Americans are injured by Musical Instruments each year?",
+                    answer1: "12000",
+                    answer2:"6000",
+                    correctAnswer: "8000",
+                    },{
+                    question : "How many steps does it take to reach the top of the Eiffel Tower?",
+                    correctAnswer : "1665",
+                    answer1 : "1710",
+                    answer2 : "1980",
+                    },{
+                    question : "What is the national animal of Scotland",
+                    answer1 : "Gyrfalcon",
+                    correctAnswer : "Unicorn",
+                    answer2 : "Chollima",
+                    },{
+                    question : "How many beaches does Australia have?",
+                    answer1 : "12,400",
+                    correctAnswer : "10,685",
+                    answer2 : "6,942",
+                    },{
+                    question : "A grasshopper can leap ____ times the length of it's body?",
+                    answer1 : "10",
+                    correctAnswer : "20",
+                    answer2 : "36",
+                    },{
+                    question : "The average iPhone user unlocks their device ___ times per day?",
+                    correctAnswer : "80",
+                    answer1 : "30",
+                    answer2 : "140",
+                    }],
 
     //Start Timer
-    function run(){
-          counter = setInterval(countdown, 1000);
-        }
+     run: function (){
+          counter = setInterval(game.countdown, 1000);
+        },
 
-    function countdown(){
-          number--
-          document.getElementById('time').innerHTML = (number)
-          if (number === 0){
-            stop();
-            results();
+     countdown: function(){
+          game.number--;
+          document.getElementById('time').innerHTML = (game.number)
+          if (game.number === 0){
+            game.stop();
+            game.results();
           }
-        }
+        },
 
     //Stop Timer
-    function stop(){
+     stop: function(){
           clearInterval(counter);
-        }
+        },
 
 
-    // Start Game Function
-    $('.start').click(function() {
-        run();
-        chooseQuestion();
-        $('.start').hide();
-    });
+  
 
 
 
     // Choose a random question from the array.
-    function chooseQuestion() {
-        $('.question').html(questions[askedQuestions].question);
-        $('.answers').html("<button class='choice' id='selection'>" + questions[askedQuestions].answer1 + "</button><br>");
-        $('.answers').append("<button class='choice' id='selection'>" + questions[askedQuestions].answer2 + "</button><br>");
-        $('.answers').append("<button class='choice' id='selection'>" + questions[askedQuestions].correctAnswer + "</button><br>");
-    }
+     chooseQuestion: function() {
+        $('.question').html(game.questions[game.askedQuestions].question);
+        $('.answers').html("<li><button id='selection1'>" + game.questions[game.askedQuestions].answer1 + "</button></li>");
+        $('.answers').append("<li><button id='selection2'>" + game.questions[game.askedQuestions].answer2 + "</button></li>");
+        $('.answers').append("<li><button id='selection3'>" + game.questions[game.askedQuestions].correctAnswer + "</button></li>");
+    },
+
+    //Show answer
+     answerDelay: function(){
+          counter = setInterval(game.delay, 1000);
+        },
+
+     delay: function(){
+          game.answerTime--;
+          document.getElementById('time').innerHTML = (answerTime)
+          if (answerTime === 0){
+            game.stop();
+            game.number=30;
+            game.chooseQuestion();
+            game.run();
+          }
+        },
 
     // Results
-    function results() {
+     results: function() {
         $('.question').hide();
         $('.answers').hide();
-        $('.results').html("<p>Correct Answers: " + correct + "</p><br>" + "<p>Incorrect Answers: " + incorrect + 
+        $('.results').html("<p>Correct Answers: " + game.correct + "</p><br>" + "<p>Incorrect Answers: " + game.incorrect + 
             "</p>")
     }
-
-
-
+    }
+  // Start Game Function
+    $('.start').click(function() {
+        game.run();
+        game.chooseQuestion();
+        $('.start').hide();
+    })
     // onclick function for buttons and answer logic.
-    $('#selection').on('click', function() {
-       if($('.choice', '.answers').val() == correctAnswer) {
-        correct ++;
+    $('.answers').on('click', 'button', function() {
+       var choice = $(this).text();
+       console.log(choice);
+       console.log(game.questions[game.askedQuestions].correctAnswer);
+       if(choice == game.questions[game.askedQuestions].correctAnswer) {
+        game.correct ++;
        } else {
-        incorrect ++;
-       }; 
-       askedQuestions ++;
+        game.incorrect ++;
+        game.stop();
+        game.answerDelay();
+        $('.answers').empty();
+        $('.answers').html("<p>Nope! The correct answer was: " + game.questions[game.askedQuestions].correctAnswer + "</p>");
+       };
+       game.askedQuestions ++;
+
 
     //Game Finishes.
-       if(askedQuestions == questions.length) {
-            stop();
+       if(game.askedQuestions == game.questions.length) {
+            game.stop();
             $('#time').html("<h3>Game Over</h3>");
-            results();
+            game.results();
        } 
        else {
-       chooseQuestion();
+        game.stop();
+        game.number=30;
+        game.chooseQuestion();
+        game.run();
         }
-    });
+    })
+});
+
+    
 
 
 
 
-    });
+
+
 
 
 
